7a5f777a1361fe5d3325ef30f9d5f20f120b4f83
Maira Bello <maira.araujo@liferay.com>
2014-05-29 17:40:22 -0300

    Preventing users from focusing outside the modal when it's open
    The user is currently able to focus on elements behind the modal, though only when they're inside iframes. This change adds code to prevent this by changing all iframes on the page to have a tabIndex value of -1. It restores the tabIndex attribute when the modal is closed.

--- src/widget-modality/js/Widget-Modality.js
+++ src/widget-modality/js/Widget-Modality.js
@@ -329,9 +329,9 @@ var WIDGET       = 'widget',
                     maskNode.show();
                     Y.later(1, this, '_attachUIHandlesModal');
                     this._focus();
+
+                    this._blockIFrameFocus();
                 }
-
-
             } else {
 
                 index = Y.Array.indexOf(stack, this);
@@ -363,7 +363,38 @@ var WIDGET       = 'widget',
 
                 }
 
+                this._unblockIFrameFocus();
             }
+        },
+
+        /**
+         * Blocks iframes on the page from getting focused by setting their
+         * tabIndex attribute to -1. The previous value of tabIndex is saved
+         * so it can be restored later.
+         *
+         * @method _blockIFrameFocus
+         * @protected
+         */
+        _blockIFrameFocus: function() {
+            Y.all('iframe').each(function() {
+                this.setData('previous-tab-index', this.get('tabIndex'));
+                this.set('tabIndex', -1);
+            });
+        },
+
+        /**
+         * Unblocks focus for the iframes on the page by restoring their original
+         * tabIndex attributes (see the _blockIFrameFocus method).
+         *
+         * @method _unblockIFrameFocus
+         * @protected
+         */
+        _unblockIFrameFocus: function() {
+            Y.all('iframe').each(function() {
+                if (this.getData('previous-tab-index') !== undefined) {
+                    this.set('tabIndex', this.getData('previous-tab-index'));
+                }
+            });
         },
 
         /**
