0811933de142c3909263531e9927533e7f894962
John Zhao <john.zhao@liferay.com>
2015-05-26 08:58:21 +0800

    AUI-1908 Only block focusing on iframes outside of bounding box.

--- src/widget-modality/js/Widget-Modality.js
+++ src/widget-modality/js/Widget-Modality.js
@@ -376,9 +376,13 @@ var WIDGET       = 'widget',
          * @protected
          */
         _blockIFrameFocus: function() {
-            Y.all('iframe').each(function() {
-                this.setAttribute('data-tabindex', this.get('tabIndex'));
-                this.set('tabIndex', -1);
+            var bb = this.get(BOUNDING_BOX);
+
+            Y.all('iframe').each(function(bb) {
+                if (!bb.contains(this)) {
+                    this.setAttribute('data-tabindex', this.get('tabIndex'));
+                    this.set('tabIndex', -1);
+                }
             });
         },
 
