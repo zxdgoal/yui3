baf634ae346e7d5b50c6e85881f5b2e520e91bb2
Jonathan Mak <jonathan.mak@liferay.com>
2016-02-17 16:50:23 -0800

    AUI-2081 - Build

--- build/node-core/node-core.js
+++ build/node-core/node-core.js
@@ -283,7 +283,7 @@ Y_Node.one = function(node) {
             if (!node) {
                 return null; // NOTE: return
             }
-        } else if (node.getDOMNode) {
+        } else if (node.getDOMNode && Y.instanceOf(node, Y_Node)) {
             return node; // NOTE: return
         }
 
